789451cf9af523f99333b095d36228e5f7bd1e03
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-05 21:12:02 +0530

    File system

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
  
-import { readFile } from 'node:fs';
-import {dirname,join} from 'node:path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'node:url'
+import {dirname, join} from 'node:path'
+import { log } from 'node:console';
+import { appendFile, readFile, writeFile } from 'node:fs';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-readFile(join(__dirname,"myname.txt"),(err,data)=>{
-    if(err){
-        console.log(err);
+
+// readFile(join(__dirname,'read.txt'),{ 
+//     encoding: 'utf-8',
+// },(err,data)=>{
+//     if(!err){
+//         log(data);
+//     }
+// });
+
+// writeFile(join(__dirname,'write.txt'),"Sabaragamuwa University of Sri Lanlka",(err)=>{
+//     if(err){
+//         log(err);
+//     }
+// });
+
+appendFile(join(__dirname,'write.txt'),
+`
+new data added
+`,
+(err)=>{
+    if(!err){
+        
+            log('document updated');
+        }
     }
-        console.log(data.toString());
-}
+    
 );
