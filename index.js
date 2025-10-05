c96ad4db34ad0f0c8528be4d1ca52ce28696feeb
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-05 22:08:47 +0530

    promise API

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
- 
-import {fileURLToPath} from 'node:url'
-import {dirname, join} from 'node:path'
-import { log } from 'node:console';
-import { appendFile, readFile, writeFile } from 'node:fs';
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { log } from "node:console";
+// import { appendFile, readFile, writeFile } from "node:fs";
+import { readFile, writeFile, appendFile, rmdir } from "node:fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/// Promise API
 
-// readFile(join(__dirname,'read.txt'),{ 
+// readFile(join(__dirname, "read.txt"), {
+//   encoding: "utf-8",
+// })
+//   .then((data) => log(data))
+//   .catch((err) => log(err));
+
+
+const filePath=join(__dirname,'write.txt');
+const content=`Hello! I am Kavishka
+This is my first file operation using promise API`;
+writeFile(filePath,content,{encoding:'utf-8'})
+.then(()=>{
+    log('file written successfully');
+})
+.catch((err)=>{
+    log("Error occured",err);
+});
+
+
+
+
+
+///// Call BAck API
+// readFile(join(__dirname,'read.txt'),{
 //     encoding: 'utf-8',
 // },(err,data)=>{
 //     if(!err){
@@ -22,15 +46,15 @@ const __dirname = dirname(__filename);
 //     }
 // });
 
-appendFile(join(__dirname,'write.txt'),
-`
-new data added
-`,
-(err)=>{
-    if(!err){
-        
-            log('document updated');
-        }
-    }
-    
-);
+// appendFile(join(__dirname,'write.txt'),
+// `
+// new data added
+// `,
+// (err)=>{
+//     if(!err){
+
+//             log('document updated');
+//         }
+//     }
+
+// );
