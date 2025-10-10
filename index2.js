f25d87da1e72e3127e34c0d71c0590c1e6296476
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-10 12:14:14 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,9 +1,9 @@
-import { log } from 'node:console'
-import {createServer} from 'node:http'
+import { log } from "node:console";
+import { createServer } from "node:http";
+import { pageSelect} from "./pageSelect.js" // ✅ ensure correct path
 
-
-createServer ((req,res)=>{
+createServer((req, res) => {
   log(req.url);
-  pageSelect(req.url,res);
-    
-}).listen(4000,()=>log('Server running'));
+  res.writeHead(200, { "Content-Type": "text/html" }); // ✅ always set response type
+  pageSelect(req.url, res);
+}).listen(4000, () => log("Server running on http://localhost:4000"));
