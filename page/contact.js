6b7ce904e009655a80dbee48a67e8d1b019bab53
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-10 12:14:49 +0530

    Create contact.js

---
+++ page/contact.js
@@ -0,0 +1,19 @@
+const Home = () => {
+  return `
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home</title>
+  </head>
+  <body>
+      <main class="main">
+        <h1>Contact Page</h1>
+      </main>
+  </body>
+  </html>
+    `;
+};
+
+export default Home;
