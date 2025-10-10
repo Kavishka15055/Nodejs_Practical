7f868d9ad25e978783c1cbf0daa28e9489c6599a
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-10 12:37:05 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -3,7 +3,6 @@ import { createServer } from "node:http";
 import { pageSelect} from "./pageSelect.js" // ✅ ensure correct path
 
 createServer((req, res) => {
-  log(req.url);
-  res.writeHead(200, { "Content-Type": "text/html" }); // ✅ always set response type
+  res.writeHead(200, { "Content-Type": "text/html" }); // ✅ always set response type,,,aniwa onema nh meka
   pageSelect(req.url, res);
 }).listen(4000, () => log("Server running on http://localhost:4000"));
