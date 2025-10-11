fa5f95956e0275fb9d21069498237ab1e8d7bf28
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-11 14:07:25 +0530

    Update read-file.js

--- Build Node Server/libs/read-file.js
+++ Build Node Server/libs/read-file.js
@@ -1,19 +1,18 @@
+// libs/read-file.js
+import { log } from "node:console";
+import { readFile } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
-import {readFile} from 'node:fs/promises'
-import { log } from "node:console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-export const fileRead = async (path)=>{
-    try{
-        const data= await readFile(join(__dirname,path))
-        return data
-    }catch(error){
-        log(error);
-        return '<h1>Server Error</h1>'
-        
-    }
-}
+export const fileRead = async (path) => {
+  try {
+    const data = await readFile(join(__dirname, path), "utf-8");
+    return data;
+  } catch (error) {
+    log(error);
+    return "<h1>Server Error</h1>";
+  }
+};
