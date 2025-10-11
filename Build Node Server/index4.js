236bc56975355a4f179c44e257a5389d6f0ea8a3
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-11 14:07:14 +0530

    Update index4.js

--- Build Node Server/index4.js
+++ Build Node Server/index4.js
@@ -1,4 +1,3 @@
-
 import { createServer } from "node:http";
 import { log } from "node:console";
 import { server } from "./server-control.js";
@@ -6,5 +5,5 @@ import { server } from "./server-control.js";
 const PORT = process.env.PORT || 4000;
 
 createServer((req, res) => {
-  server(req,res);
+  server(req, res);
 }).listen(PORT, () => log(`✅ Server running on http://localhost:${PORT}`));
