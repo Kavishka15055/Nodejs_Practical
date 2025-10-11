34813502cb9884e67537a1037506ce00db855c13
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-11 14:07:36 +0530

    Update server-control.js

--- Build Node Server/server-control.js
+++ Build Node Server/server-control.js
@@ -1,8 +1,22 @@
+// server-control.js
 import { fileRead } from "./libs/read-file.js";
 
-export const server =(req,res)=>{
-    if(req.url === '/' && req.method === 'GET'){
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(fileRead('public/index4.html'));
-    }
+export const server = async (req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    // Serve HTML
+    const html = await fileRead("../../public/index4.html");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+
+  } else if (req.url === "/style.css" && req.method === "GET") {
+    // Serve CSS
+    const css = await fileRead("../../public/style.css");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(css);
+
+  } else {
+    // Handle 404
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 Not Found</h1>");
+  }
 };
