28c01a00e9567b3c41a3c451e9838873a930c07c
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-12 18:26:22 +0530

    Update index5.js

--- Work With EventEmitter/index5.js
+++ Work With EventEmitter/index5.js
@@ -1,17 +1,26 @@
-import { EventEmitter } from 'node:events';
-import { log } from 'node:console';
+import { EventEmitter } from "node:events";
+import { log } from "node:console";
 
 class AUKEmitter extends EventEmitter {}
 
 const emiterObj = new AUKEmitter();
 
-emiterObj.on('onClick', (name,city) => {
-    log('onClick event',`Your name is : ${name} ${city.name}`);
+emiterObj.on("onClick", (name, city) => {
+  log("onClick event", `Your name is : ${name} ${city.name}`);
 });
 
-emiterObj.on('onClick', () => {
-    log('onClick event');
-});
 
+const myFunction = async (name) => {
+  log("onClick event",name);
+}
+
+emiterObj.addListener("onClick2", myFunction);
+
+emiterObj.emit("onClick2",'Amila');
+emiterObj.emit("onClick2",'Upul');
+emiterObj.removeListener("onClick2",myFunction);
+emiterObj.emit("onClick2");
+emiterObj.emit("onClick2");
+emiterObj.emit("onClick2");
 
 emiterObj.emit('onClick',12,{name:"kamal"});
