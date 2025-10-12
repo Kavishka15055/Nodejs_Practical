d54d32436725a3b7063156741304ccc20fb71a8b
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-12 18:43:24 +0530

    Update index5.js

--- Work With EventEmitter/index5.js
+++ Work With EventEmitter/index5.js
@@ -1,26 +1,35 @@
 import { EventEmitter } from "node:events";
 import { log } from "node:console";
 
-class AUKEmitter extends EventEmitter {}
+class AUKEmitter extends EventEmitter {
+    constructor(city, age){
+        super();
+        this.city = city;
+        this.age = age;
+    }
 
-const emiterObj = new AUKEmitter();
+    runOnClick(){
+       this.emit('onClick', 12, this.city); // Fixed: use this.emit, not this.emiterObj.emit
+    }
+}
+
+const emiterObj = new AUKEmitter('Horana', 24);
 
 emiterObj.on("onClick", (name, city) => {
-  log("onClick event", `Your name is : ${name} ${city.name}`);
+  log("onClick event", `Your name is : ${name} ${city}`);
 });
 
-
 const myFunction = async (name) => {
-  log("onClick event",name);
-}
+  log("onClick event", name);
+};
 
 emiterObj.addListener("onClick2", myFunction);
 
-emiterObj.emit("onClick2",'Amila');
-emiterObj.emit("onClick2",'Upul');
-emiterObj.removeListener("onClick2",myFunction);
-emiterObj.emit("onClick2");
-emiterObj.emit("onClick2");
-emiterObj.emit("onClick2");
+emiterObj.emit("onClick2", 'Amila');
+emiterObj.emit("onClick2", 'Upul');
+emiterObj.removeListener("onClick2", myFunction);
+emiterObj.emit("onClick2"); 
 
-emiterObj.emit('onClick',12,{name:"kamal"});
+emiterObj.runOnClick();
+
+
