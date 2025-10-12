b8a1f35ef84abba41c910f5b6ebdddbf4b4cc269
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-12 17:45:47 +0530

    Update index5.js

--- Work With EventEmitter/index5.js
+++ Work With EventEmitter/index5.js
@@ -5,8 +5,13 @@ class AUKEmitter extends EventEmitter {}
 
 const emiterObj = new AUKEmitter();
 
-emiterObj.on('amila', () => {
-    log('amila event');
+emiterObj.on('onClick', (name,city) => {
+    log('onClick event',`Your name is : ${name} ${city.name}`);
 });
 
-emiterObj.emit('amila');
+emiterObj.on('onClick', () => {
+    log('onClick event');
+});
+
+
+emiterObj.emit('onClick',12,{name:"kamal"});
