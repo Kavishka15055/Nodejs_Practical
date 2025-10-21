739986674a5694b2e755016f93b4b2b7e9259507
Kavishka15055 <kavishkapiyumal22@gmail.com>
2025-10-21 14:07:47 +0530

    Update index.js

--- databaseMysql/index.js
+++ databaseMysql/index.js
@@ -1,26 +1,36 @@
-import { createConnection} from "mysql2";
-import { log} from "node:console";
+import { createConnection } from "mysql2";
+import { log } from "node:console";
 
-const db =createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "",
-    database: "school"
+const db = createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "school"
 });
 
-db.connect((err)=>{
-    if(err){
-        log(err)
-    }else{ 
-        log("db connected")
-        const sqlQuery="SELECT * FROM `student_details`";
-        db.query(sqlQuery,(err,result)=>{
-            if(err){
-                log(err);
-            }else{
-                log(result);
-            }
-        })
-    }
-})
+db.connect((err) => {
+  if (err) {
+    log("Connection error:", err);
+  } else {
+    log("DB connected");
+
+    const sqlQuery = `
+      CREATE TABLE IF NOT EXISTS office_details (
+        id INT AUTO_INCREMENT,
+        name VARCHAR(50),
+        phone VARCHAR(50),
+        PRIMARY KEY (id)
+      )
+    `;
+
+    db.query(sqlQuery, (err, result) => {
+      if (err) {
+        log("Query error:", err);
+      } else {
+        log("Table created or already exists:", result);
+      }
+      db.end();
+    });
+  }
+});
